3e3429559caf52375ca30124164b4c931ecb0910
firegene <37655585+firegene@users.noreply.github.com>
2018-07-19 20:00:39 +0100

    Update bot.js

--- bot/bot.js
+++ bot/bot.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-let ms = require(./minestat);
+let ms = require("./minestat");
 let prefix = ".";
 
 client.on("ready", () => {
