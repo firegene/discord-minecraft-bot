ea962224b7b21824c961af41dc9876bdfdf18826
firegene <37655585+firegene@users.noreply.github.com>
2018-07-19 19:52:18 +0100

    Update bot.js

--- bot/bot.js
+++ bot/bot.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-let prefix = "."
+let ms = require(./minestat);
+let prefix = ".";
 
 client.on("ready", () => {
   console.log("Logged in as ${client.user.tag}");
@@ -12,7 +13,20 @@ client.on("message", (message) => {
  const command = args.shift().toLowerCase();
  
   if (command === "server") {
-    
+    ms.init('198.50.141.83', 25565, function(result)
+{
+  console.log("Minecraft server status of " + ms.address + " on port " + ms.port + ":");
+  if(ms.online)
+  {
+    /*console.log("Server is online running version " + ms.version + " with " + ms.current_players + " out of " + ms.max_players + " players.");
+    console.log("Message of the day: " + ms.motd);*/
+    msg.channel.send("**Server status**: online\nServer version: ${ms.version}")
+  }
+  else
+  {
+    console.log("Server is offline!");
+  }
+});
   }
 });
 
