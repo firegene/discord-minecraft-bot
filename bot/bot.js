9c1a934334dd0e97de161f5b60e26d61a941f838
firegene <37655585+firegene@users.noreply.github.com>
2018-07-20 15:03:57 +0100

    Update bot.js

--- bot/bot.js
+++ bot/bot.js
@@ -1,19 +1,20 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 let ms = require("./minestat");
+const snekfetch= require('snekfetch'); 
 let prefix = ".";
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}`);
 });
 
-client.on("message", (msg) => {
+client.on("message", async (msg) => {
 
  const args = msg.content.slice(prefix.length).trim().split(/ +/g);
  const command = args.shift().toLowerCase();
  
   if (command === "server") {
-    ms.init('198.50.141.83', 25565, function(result)
+    ms.init('ip', port, function(result)
 {
   console.log("Minecraft server status of " + ms.address + " on port " + ms.port + ":");
   if(ms.online)
@@ -25,7 +26,18 @@ client.on("message", (msg) => {
     msg.channel.send("Server is offline!");
   }
 });
-  }
+  };
+
+if(command === "phistory") {
+  const res = await snekfetch.get(`https://api.mojang.com/users/profiles/minecraft/${args[0]}`);
+  let id = res.body.id
+  const his = await snekfetch.get(`https://api.mojang.com/user/profiles/${id}/names`);
+  let map = his.body.map(o => o.name + ' : [' + (o.changedToAt === undefined ? 'CURRENT NAME' : new Date(o.changedToAt).toDateString()) + ']')
+  msg.channel.send(map)
+    .catch(error => {
+    msg.channel.send("The username provided is incorrect.")
+  });
+};
 });
 
 client.login('token');
