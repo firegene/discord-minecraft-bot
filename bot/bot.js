fc85135618dd8832c2ff5981f82433a337edaf36
firegene <37655585+firegene@users.noreply.github.com>
2018-07-22 15:40:28 +0100

    Update bot.js

--- bot/bot.js
+++ bot/bot.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 let ms = require("./minestat");
-const snekfetch= require('snekfetch'); 
+const snekfetch = require('snekfetch');
 let prefix = ".";
 
 client.on("ready", () => {
@@ -9,36 +9,30 @@ client.on("ready", () => {
 });
 
 client.on("message", async (msg) => {
- if (msg.author.bot) return;
- if (msg.content.indexOf(prefix) !== 0) return;
- const args = msg.content.slice(prefix.length).trim().split(/ +/g);
- const command = args.shift().toLowerCase();
- 
-  if (command === "server") {
-    ms.init('198.50.141.83', 25565, function(result)
-{
-  console.log("Minecraft server status of " + ms.address + " on port " + ms.port + ":");
-  if(ms.online)
-  {
-    msg.channel.send(`**Server status:** online\n**Server version:** ${ms.version}\n**Players:** ${ms.current_players}/${ms.max_players}\n**Server icon:**`, new Discord.Attachment('https://api.minetools.eu/favicon/198.50.141.83/25565', 'file.jpg'));
-  }
-  else
-  {
-    msg.channel.send("Server is offline!");
-  }
-});
-  };
+  if (msg.author.bot) return;
+  if (msg.content.indexOf(prefix) !== 0) return;
+  const args = msg.content.slice(prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
 
-if(command === "phistory") {
-  const res = await snekfetch.get(`https://api.mojang.com/users/profiles/minecraft/${args[0]}`);
-  let id = res.body.id
-  const his = await snekfetch.get(`https://api.mojang.com/user/profiles/${id}/names`);
-  let map = his.body.sort().map(o => o.name + ' : [' + (o.changedToAt === undefined ? 'CURRENT NAME' : new Date(o.changedToAt).toDateString()) + ']')
-  msg.channel.send(map)
-    .catch(error => {
-    msg.channel.send("The username provided is incorrect.")
-  });
-};
+  if (command === "server") {
+    ms.init('198.50.141.83', 25565, function(result) {
+      console.log("Minecraft server status of " + ms.address + " on port " + ms.port + ":");
+      if (ms.online) {
+        msg.channel.send(`**Server status:** online\n**Server version:** ${ms.version}\n**Players:** ${ms.current_players}/${ms.max_players}\n**Server icon:**`, new Discord.Attachment('https://api.minetools.eu/favicon/198.50.141.83/25565', 'file.jpg'));
+      } else {
+        msg.channel.send("Server is offline!");
+      }
+    });
+  }else if (command === "phistory") {
+    const res = await snekfetch.get(`https://api.mojang.com/users/profiles/minecraft/${args[0]}`);
+    let id = res.body.id
+    const his = await snekfetch.get(`https://api.mojang.com/user/profiles/${id}/names`);
+    let map = his.body.sort().map(o => o.name + ' : [' + (o.changedToAt === undefined ? 'CURRENT NAME' : new Date(o.changedToAt).toDateString()) + ']')
+    msg.channel.send(map)
+      .catch(error => {
+        msg.channel.send("The username provided is incorrect.")
+      });
+  };
 });
 
 client.login('token');
