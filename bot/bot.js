56ec0859e2aefd072af5acc621eb86683200b90b
Ciarán Mc Aree <37655585+firegene@users.noreply.github.com>
2018-07-28 11:39:33 +0100

    Added vote, ban and kick commands.
    Server IP was also added to the server info embed.

--- bot/bot.js
+++ bot/bot.js
@@ -1,11 +1,12 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 let ms = require("./minestat");
-const snekfetch = require('snekfetch'); 
+const snekfetch = require('snekfetch');
 let prefix = "!";
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}`);
+
 });
 
 client.on("message", async (msg) => {
@@ -24,7 +25,7 @@ client.on("message", async (msg) => {
 {
   console.log("Minecraft server status of " + ms.address + " on port " + ms.port + ":");
   if(ms.online)
-  {
+  { 
     msg.channel.send({
   "embed": {
     "url": "https://discordapp.com",
@@ -40,6 +41,10 @@ client.on("message", async (msg) => {
       {
         "name": "Server status",
         "value": "Online ✓"
+      },
+      {
+        "name": "Server IP",
+        "value": "play.swancraftmc.com"
       },
       {
         "name": "Server version",
@@ -66,7 +71,6 @@ if(command === "phistory") {
   const his = await snekfetch.get(`https://api.mojang.com/user/profiles/${id}/names`);
   let map = his.body.reverse().map(o => o.name + ' : [' + (o.changedToAt === undefined ? 'ORIGINAL NAME' : new Date(o.changedToAt).toDateString()) + ']')
   let newmap = map.join('\n')
-  //msg.channel.send(map)
   msg.channel.send({
   "embed": {
     "color": 1234643,
@@ -83,6 +87,57 @@ if(command === "phistory") {
     msg.channel.send("The username provided is incorrect.")
   });
 };
+  
+if(command === "vote") {
+msg.channel.send({
+  "embed": {
+    "color": 1234643,
+    "author": {
+      "name": "SwancraftMC Voting",
+      "url": "https://www.swancraftmc.com/vote"
+    },
+    "fields": [
+      {
+        "name": "Vote links",
+        "value": "[Planet Minecraft](https://planetminecraft.com/server/swancraft-3882768/vote/)\n[Minecraft Servers](http://minecraftservers.org/vote/410424)\n[Minecraft Server List](http://minecraft-server-list.com/server/389267/vote/)\n[TopG](http://topg.org/Minecraft/in-449700)\n[Minecraft-Server](https://goo.gl/VeyXyQ)\n[Minecraft MP](http://minecraft-mp.com/server/145239/vote/)\n[Minecraft Forum](https://minecraftservers.biz/servers/73758/)"
+      }
+    ]  
+  }
+})
+}
+  
+if (command === 'ban') {
+let argMember; 
+try{
+   argMember = await msg.guild.fetchMember(args[0])
+} catch(err) {
+    msg.channel.send("That user can not be found")
+    return;
+};
+
+  if(msg.member.hasPermission('BAN_MEMBERS') && argMember.bannable && msg.member.highestRole.comparePositionTo(argMember.highestRole) > 0) {
+    msg.guild.ban(argMember);
+  } else {
+    msg.channel.send("Someone doesn't have enough permissions :thinking:");
+};
+}
+  
+if (command === 'kick') {
+let argMember; 
+try{
+   argMember = await msg.guild.fetchMember(args[0])
+} catch(err) {
+    msg.channel.send("That user can not be found.")
+    return;
+};
+
+  if(msg.member.hasPermission('KICK_MEMBERS') && argMember.kickable && msg.member.highestRole.comparePositionTo(argMember.highestRole) > 0) {
+    msg.channel.send(`I have kicked ${argMember.displayName}.`)
+    argMember.kick();
+  } else {
+    msg.channel.send("Someone doesn't have enough permissions :thinking:");
+};
+}
 });
 
 client.login('token');
